fbf0058c45f2eb6785f48634ca08acc43513ea2d
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-06-11 03:11:41 -0700

    First commit and project creation and some configs

---
+++ src/appwrite/config.ts
@@ -0,0 +1,13 @@
+import conf from "@/conf/config";
+import { Client, Account, ID } from "appwrite";
+
+type CreateUserAccount = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+type LoginUserAccount = {
+  email: string;
+  password: string;
+};
---
+++ src/conf/config.ts
@@ -0,0 +1,5 @@
+const conf = {
+  appwriteUrl: String(process.env.NEXT_PUBLIC_APPWRITE_URL),
+  appwriteProjectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+};
+export default conf;
