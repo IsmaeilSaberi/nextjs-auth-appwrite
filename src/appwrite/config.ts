862034cca10c345f0aa63cbe124a1398aa8c9d4f
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-06-13 23:21:15 -0700

    work on some logic and context

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -11,3 +11,64 @@ type LoginUserAccount = {
   email: string;
   password: string;
 };
+
+const appwriteClient = new Client();
+
+appwriteClient.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+
+export const account = new Account(appwriteClient);
+
+export class AppwriteService {
+  // create a new record of user inside appweite
+  async createUserAccount({ email, password, name }: CreateUserAccount) {
+    try {
+      const userAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error: any) {
+      throw error;
+    }
+  }
+  async login({ email, password }: LoginUserAccount) {
+    try {
+      return await account.createEmailPasswordSession(email, password);
+    } catch (error: any) {
+      throw error;
+    }
+  }
+
+  async isLoggedIn(): Promise<boolean> {
+    try {
+      const data = await this.getCurrentUser();
+      return Boolean(data);
+    } catch (error) {}
+    return false;
+  }
+  async getCurrentUser() {
+    try {
+      return await account.get();
+    } catch (error) {
+      console.log("getcurrentuser error: " + error);
+    }
+  }
+
+  async logout() {
+    try {
+      return await account.deleteSession("current");
+    } catch (error) {
+      console.log("logout error: " + error);
+    }
+  }
+}
+
+const appWriteService = new AppwriteService();
+
+export default appWriteService;
---
+++ src/context/authContext.ts
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext<{
+  authStatus: boolean;
+  setAuthStatus: (status: boolean) => void;
+}>({ authStatus: false, setAuthStatus: () => {} });
+
+export const AuthProvider = AuthContext.Provider;
+
+export default AuthContext;
